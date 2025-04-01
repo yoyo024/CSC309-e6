6b3bff66289cd067736e02347ebd5c6dd68ed8d6
yoyo024 <126288329+yoyo024@users.noreply.github.com>
2025-03-31 23:00:07 -0400

    upload new app

---
+++ app/csc309-e11/page.js
@@ -0,0 +1,8 @@
+export default function e11() {
+    return (
+      <div id="auto-tester">
+        Hi. I am lizhidon, and I have learned how to develop a full-stack web app
+        and deploy it!
+      </div>
+    );
+  }
